cf40232d216c8d0cac10b37acb5643f68ec8503f
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-11-14 18:57:25 -0300

    Starting interfaces

---
+++ menulateral/sidebar/src/components/SidebarMenu..tsx
@@ -0,0 +1,10 @@
+import { SidebarMenuCard, SidebarMenuItem } from "../types/types";
+
+interface SidebarMenuProps{
+    items:SidebarMenuItem[];
+    card:SidebarMenuCard;
+
+}
+export function SidebarMenu({items,card}: SidebarMenuProps){
+    return <div>Hola</div>;
+}
