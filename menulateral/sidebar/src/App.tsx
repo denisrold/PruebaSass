dbe1a7b7df603bae91c6902bfbf261730ae4b5c0
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-11-16 18:37:09 -0300

    SideBarMenu

--- menulateral/sidebar/src/App.tsx
+++ menulateral/sidebar/src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css';
 import { SidebarMenu } from './components/SidebarMenu.';
+import { SidebarMenuCard, SidebarMenuItem } from './types/types';
+import {FcAdvertising} from 'react-icons/fc'
+
 function App() {
+  const items:SidebarMenuItem[] = [
+    {id:'1',
+    label:'Hola',
+    icon: FcAdvertising,
+    url:'/'}
+  ]
+
+  const card:SidebarMenuCard = {
+    id:'card01',
+    displayName:'Marcos Rivas',
+    title:'youtuber',
+    photoUrl:'',
+    url: '/', 
+  }
   return (
     <div>
-      <SidebarMenu />
+      <SidebarMenu items={items} card={card}/>
     </div>
   );
 }
--- menulateral/sidebar/src/components/SidebarMenu..tsx
+++ menulateral/sidebar/src/components/SidebarMenu..tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { SidebarMenuCard, SidebarMenuItem } from "../types/types";
+import {classNames} from "../util/classes";
+import { VscMenu } from "react-icons/vsc";
 
 interface SidebarMenuProps{
     items:SidebarMenuItem[];
@@ -6,5 +9,13 @@ interface SidebarMenuProps{
 
 }
 export function SidebarMenu({items,card}: SidebarMenuProps){
-    return <div>Hola</div>;
+    const [isOpen,setIsOpen] = useState<boolean>(true); 
+
+    return <div className={classNames("SideBarMenu", isOpen? "expanded":"collapsed")}>
+            <div className="menuButton">
+                <button>
+                    <VscMenu />
+                </button>
+            </div>
+        </div>;
 }
