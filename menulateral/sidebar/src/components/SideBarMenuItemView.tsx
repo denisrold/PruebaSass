c2b2b03c628c4cf5b96cdf6caad4565f252b4877
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-11-16 19:20:35 -0300

    sideBar menu item view ok

--- menulateral/sidebar/src/components/SideBarMenuItemView.tsx
+++ menulateral/sidebar/src/components/SideBarMenuItemView.tsx
@@ -1,3 +1,21 @@
-export function SideBarMenuItemView(props:any){
-   return <div>SideBarMenuItemView</div> 
+import { SidebarMenuItem } from "../types/types";
+import { classNames } from "../util/classes";
+
+interface SideBarMenuItemViewProps{
+    item: SidebarMenuItem,
+    isOpen: boolean;
+}
+
+export function SideBarMenuItemView({item, isOpen}:SideBarMenuItemViewProps){
+   return <div className="SidebarMenuItem">
+    <a href={item.url}>
+        <div className={classNames('ItemContent', isOpen? '':'collapsed')}>
+            <div className="icon">
+                <item.icon size='32'/>
+            </div>
+            <span className="label">{item.label}</span>
+        </div>
+    </a>
+        {!isOpen ? <div className='tooltip'>{item.label}</div>:''}
+   </div> 
 }
